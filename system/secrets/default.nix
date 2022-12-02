83200c90d24f08a5a6d40d473fa10b5cc5df12e1
Mark Feller <mark@mfeller.io>
2022-12-02 15:58:48 -0700

    move secrets

--- system/secrets/default.nix
+++ system/secrets/default.nix
@@ -3,7 +3,7 @@
   pkgs,
   ...
 }: {
-  sops.defaultSopsFile = ../../secrets/secrets.yaml;
+  sops.defaultSopsFile = ./secrets.yaml;
   sops.age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
