0034be8636a7a07f4a910209abced62a9af782d2
Mark Feller <mark@mfeller.io>
2023-05-13 18:39:32 -0600

    add emacs packages to desktop host

--- hosts/P2X-3YZ/system.nix
+++ hosts/P2X-3YZ/system.nix
@@ -74,7 +74,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    emacsUnstable
     age
     bat
     file
@@ -111,6 +110,54 @@
 
     bazel_6
     bazelisk
+
+    ((emacsPackagesFor emacsUnstable).emacsWithPackages (epkgs: [
+      epkgs.bind-key
+      epkgs.company
+      epkgs.company-c-headers
+      epkgs.comment-dwim-2
+      epkgs.consult
+      epkgs.csv-mode
+      epkgs.direnv
+      epkgs.dockerfile-mode
+      epkgs.elfeed
+      epkgs.embark
+      epkgs.embark-consult
+      epkgs.evil
+      epkgs.evil-goggles
+      epkgs.evil-surround
+      epkgs.ggtags
+      epkgs.git-timemachine
+      epkgs.go-mode
+      epkgs.helpful
+      epkgs.highlight-indentation
+      epkgs.ledger-mode
+      epkgs.logos
+      epkgs.macrostep
+      epkgs.magit
+      epkgs.minions
+      epkgs.modus-themes
+      epkgs.multi-vterm
+      epkgs.multiple-cursors
+      epkgs.nix-mode
+      epkgs.notmuch
+      epkgs.olivetti
+      epkgs.orderless
+      epkgs.org-bullets
+      epkgs.org-fancy-priorities
+      epkgs.paren-face
+      epkgs.racer
+      epkgs.restclient
+      epkgs.rg
+      epkgs.rust-mode
+      epkgs.tree-sitter
+      epkgs.tree-sitter-langs
+      epkgs.use-package
+      epkgs.vertico
+      epkgs.vterm
+      epkgs.yaml-mode
+      epkgs.yasnippet
+    ]))
   ];
 
   fonts.fonts = [pkgs.comic-mono];
