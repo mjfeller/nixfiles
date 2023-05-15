e169a07233b6f7b796e18d293b19a3f668f7d4e0
Mark Feller <mark@mfeller.io>
2023-05-15 12:41:03 -0600

    add macos defaults for dock and trackpad

--- system/macos/default.nix
+++ system/macos/default.nix
@@ -14,6 +14,19 @@
   system.keyboard.enableKeyMapping = true;
   system.keyboard.remapCapsLockToControl = true;
 
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.mineffect = "scale";
+  system.defaults.dock.minimize-to-application = true;
+  system.defaults.dock.orientation = "left";
+  system.defaults.dock.show-process-indicators = false;
+  system.defaults.dock.show-recents = false;
+  system.defaults.dock.static-only = true;
+  system.defaults.dock.tilesize = 64;
+
+  system.defaults.trackpad.Clicking = true;
+  system.defaults.trackpad.Dragging = true;
+  system.defaults.trackpad.TrackpadThreeFingerDrag = true;
+
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
