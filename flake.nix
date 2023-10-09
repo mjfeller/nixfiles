b4885fc649ab3e06564d7a3c4dc2ad51e6ec6458
Mark Feller <mark@mfeller.io>
2023-10-08 18:01:15 -0600

    cleanup flake.nix

--- flake.nix
+++ flake.nix
@@ -33,13 +33,15 @@
   } @ inputs: {
     formatter."x86_64-linux" = nixpkgs.legacyPackages."x86_64-linux".alejandra;
 
-    # Linux
-    nixosConfigurations.P2X-3YZ = import ./hosts/P2X-3YZ inputs; # P2X-3YZ -- Desktop
-    nixosConfigurations.P3R-272 = import ./hosts/P3R-272 inputs; # P3R-272 -- Media Fetcher
-    nixosConfigurations.P4X-347 = import ./hosts/P4X-347 inputs; # P4X-347 -- Media Player Server
+    nixosConfigurations = {
+      P2X-3YZ = import ./hosts/P2X-3YZ inputs; # P2X-3YZ -- Desktop
+      P3R-272 = import ./hosts/P3R-272 inputs; # P3R-272 -- Media Fetcher
+      P4X-347 = import ./hosts/P4X-347 inputs; # P4X-347 -- Media Player Server
+    };
 
-    # macOS
-    darwinConfigurations.p2s-4c3 = import ./hosts/P2S-4C3 inputs; # P2S-4C3 -- Work Laptop MacBook Pro M1
-    darwinConfigurations.p3x-984 = import ./hosts/P3X-984 inputs; # P3X-984 -- 16" MacBook Pro 2019
+    darwinConfigurations = {
+      p2s-4c3 = import ./hosts/P2S-4C3 inputs; # P2S-4C3 -- Work Laptop MacBook Pro M1
+      p3x-984 = import ./hosts/P3X-984 inputs; # P3X-984 -- 16" MacBook Pro 2019
+    };
   };
 }
