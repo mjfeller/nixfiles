8b78f09d73d2ff9df7c6c926e189bbfc606fd570
Mark Feller <mark@mfeller.io>
2023-12-26 20:09:50 -0700

    home: fix prompts and xdg desktop portals

--- hosts/P2X-3YZ/system.nix
+++ hosts/P2X-3YZ/system.nix
@@ -104,6 +104,7 @@
     bemenu
     jq
     ledger
+    xdg-utils
   ];
 
   programs.steam.enable = true;
@@ -149,4 +150,7 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11";
+
+  xdg.portal.enable = true;
+  xdg.portal.wlr.enable = true;
 }
