2b5d24f42b1c06e6663ae0e7ebb7bb369fb96c80
Mark Feller <mfeller@squareup.com>
2024-03-12 17:35:55 -0600

    hosts: add fonts to work macbook

--- hosts/P2S-4C3/system.nix
+++ hosts/P2S-4C3/system.nix
@@ -76,7 +76,6 @@
     jq
     lf
     mediainfo
-    nerdfonts
     ripgrep
     rust-analyzer
     rustc
@@ -84,6 +83,23 @@
     shellcheck
     tree
     watch
+  ];
+
+  fonts.fonts = with pkgs; [
+    nerdfonts
+    iosevka
+    iosevka-comfy.comfy
+    iosevka-comfy.comfy-duo
+    iosevka-comfy.comfy-wide
+    iosevka-comfy.comfy-fixed
+    iosevka-comfy.comfy-motion
+    iosevka-comfy.comfy-wide-duo
+    iosevka-comfy.comfy-wide-fixed
+    iosevka-comfy.comfy-motion-duo
+    iosevka-comfy.comfy-motion-fixed
+    iosevka-comfy.comfy-wide-motion
+    iosevka-comfy.comfy-wide-motion-duo
+    iosevka-comfy.comfy-wide-motion-fixed
   ];
 
   # ZSH Basic Configuration
