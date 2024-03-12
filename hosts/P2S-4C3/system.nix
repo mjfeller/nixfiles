a549054d8cdb2960a47977fdb89e9af90cf46f06
Mark Feller <mfeller@squareup.com>
2024-03-12 17:56:16 -0600

    system: move fonts to a general package

--- hosts/P2S-4C3/system.nix
+++ hosts/P2S-4C3/system.nix
@@ -5,6 +5,7 @@
 }: {
   imports = [
     ../../system/emacs
+    ../../system/fonts
     ../../system/macos
   ];
 
@@ -90,23 +91,6 @@
     watch
     zig
     zls
-  ];
-
-  fonts.fonts = with pkgs; [
-    nerdfonts
-    iosevka
-    iosevka-comfy.comfy
-    iosevka-comfy.comfy-duo
-    iosevka-comfy.comfy-wide
-    iosevka-comfy.comfy-fixed
-    iosevka-comfy.comfy-motion
-    iosevka-comfy.comfy-wide-duo
-    iosevka-comfy.comfy-wide-fixed
-    iosevka-comfy.comfy-motion-duo
-    iosevka-comfy.comfy-motion-fixed
-    iosevka-comfy.comfy-wide-motion
-    iosevka-comfy.comfy-wide-motion-duo
-    iosevka-comfy.comfy-wide-motion-fixed
   ];
 
   # ZSH Basic Configuration
---
+++ system/fonts/default.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  fonts.fonts = with pkgs; [
+    nerdfonts
+    iosevka
+    iosevka-comfy.comfy
+    iosevka-comfy.comfy-duo
+    iosevka-comfy.comfy-wide
+    iosevka-comfy.comfy-fixed
+    iosevka-comfy.comfy-motion
+    iosevka-comfy.comfy-wide-duo
+    iosevka-comfy.comfy-wide-fixed
+    iosevka-comfy.comfy-motion-duo
+    iosevka-comfy.comfy-motion-fixed
+    iosevka-comfy.comfy-wide-motion
+    iosevka-comfy.comfy-wide-motion-duo
+    iosevka-comfy.comfy-wide-motion-fixed
+  ];
+}
