120c405bce3495b85ea6a7dfcf4e15f910d070c9
Mark Feller <mark@mfeller.io>
2024-03-29 17:29:55 -0600

    update trusted public keys with cachix keys

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -16,6 +16,9 @@
     "https://nix-community.cachix.org"
     "https://cache.nixos.org/"
   ];
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
 
   environment.systemPackages = with pkgs; [
     comma
