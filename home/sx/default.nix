743c6231b67e79b2d4de1601e54fcb541d7d773d
Mark Feller <mark@mfeller.io>
2024-03-31 15:02:56 -0600

    home: remove unused home packages

--- home/sx/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  # sx is a simple alternative to both xinit(1) and startx(1) for
-  # starting an Xorg server.
-  xdg.configFile."sx/sxrc".source = pkgs.writeShellScript "sxrc" "exec dbus-launch dwm";
-}
