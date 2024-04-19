186041b76f9099c495914e73c6f5dffb2a94cb71
Mark Feller <mark@mfeller.io>
2024-04-18 20:50:43 -0600

    hosts: add basic nix configuration to servers

--- hosts/P3R-272/system.nix
+++ hosts/P3R-272/system.nix
@@ -12,6 +12,15 @@
     ../../system/users
   ];
 
+  # Add in the experimental nix command line tool and flakes. The nix
+  # command 'should' be in the next release of nixos. As for when flakes
+  # become mainlined who knows.
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.trusted-users = ["root" "mjf"];
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 30d";
+  nix.package = pkgs.nix;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
