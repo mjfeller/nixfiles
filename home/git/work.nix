3ca33ea32fd79c0a40692a8109b53cc6b7201be2
Mark Feller <mfeller@squareup.com>
2024-12-21 12:20:20 -0700

    home: temporarily disable git signing on work laptop

--- home/git/work.nix
+++ home/git/work.nix
@@ -8,7 +8,7 @@
     userName = "Mark Feller";
     userEmail = "mark@block.xyz";
     signing.key = "AD1123AE40116CBA25464FA10B54905CC58C10EE";
-    signing.signByDefault = true;
+    signing.signByDefault = false;
 
     extraConfig = {
       github.user = "mfeller-square";
