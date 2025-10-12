fb5dbb752798530681a535ba64343191ba733dab
Mark Feller <mark@mfeller.io>
2025-10-12 16:35:18 -0600

    Update wireguard

--- system/networking/wireguard.nix
+++ system/networking/wireguard.nix
@@ -5,15 +5,15 @@
 }: {
   networking.wg-quick.interfaces = {
     mullvad0 = {
-      address = ["10.66.156.220/32" "fc00:bbbb:bbbb:bb01::3:9cdb/128"];
+      address = ["10.68.134.9/32" "fc00:bbbb:bbbb:bb01::5:8608/128"];
       dns = ["10.64.0.1"];
       privateKeyFile = "/etc/privatekey-mullvad";
       postUp = "ip rule add from 192.168.0.0/16 table main";
       peers = [
         {
-          publicKey = "Qn1QaXYTJJSmJSMw18CGdnFiVM0/Gj/15OdkxbXCSG0=";
+          publicKey = "74U+9EQrMwVOafgXuSp8eaKG0+p4zjSsDe3J7+ojhx0=";
           allowedIPs = ["0.0.0.0/0" "::0/0"];
-          endpoint = "193.138.218.220:51820";
+          endpoint = "37.19.210.1:51820";
           persistentKeepalive = 25;
         }
       ];
